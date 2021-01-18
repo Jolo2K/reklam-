797d1e796c7c35d04f039f2fe94a4373f5f5beb6
Jolo2K <74709762+Jolo2K@users.noreply.github.com>
2021-01-18 13:13:37 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 4; index++) {
-        basic.showString("Empo2K")
+    for (let index = 0; index < 2; index++) {
+        basic.showString("Empo2K and Jolo2K are the best!")
     }
 })
 basic.forever(function () {
-	
+    for (let index = 0; index < 4; index++) {
+        led.setBrightness(255)
+        basic.showIcon(IconNames.Duck)
+        basic.pause(500)
+        led.setBrightness(55)
+        basic.showIcon(IconNames.Duck)
+        basic.pause(500)
+    }
+    basic.clearScreen()
+    basic.pause(500)
+    led.setBrightness(255)
+    for (let index = 0; index < 2; index++) {
+        basic.showNumber(input.temperature())
+        basic.pause(200)
+    }
+    basic.pause(500)
 })
